d3236e23492d480e24ec021d7b8158612c3b136e
Uwise Mohamed <peakyblinder@Uwises-MacBook-Pro.local>
2019-08-12 18:17:39 -0700

    Added toggle theme

--- app/index.js
+++ app/index.js
@@ -1,33 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import './components/popular'
-import Popular from './components/popular';
-import Battle from './components/Battle';
+import Popular from './components/Popular'
+import Battle from './components/Battle'
+import { ThemeProvider } from './contexts/theme'
+import Nav from './components/Nav'
 
-//What does a component have? They have their own states. And they manage em.
-//They also have LifeCycle
-//Then, UI
-// function uwise(){
-//     return false
-// }
-class App extends React.Component{
-    render(){
-        // const name='UWISE'
-        return (
-        // <div>
-        //     {uwise()===true?<h3>HELLO {name}</h3>: <h3>WTF {name}</h3>}
-        //     <p>Today's date: {new Date().toLocaleString()}</p>
-        // </div>
-        // <Popular/>
-        <div className='container'>
-            <Battle/>
-        </div>
-        )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
     }
+  }
+  render() {
+    return (
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className='container'>
+            <Nav />
+
+            <Popular />
+          </div>
+        </div>
+      </ThemeProvider>
+    )
+  }
 }
 
 ReactDOM.render(
-<App/>,
-document.getElementById('app')
+  <App />,
+  document.getElementById('app')
 )
